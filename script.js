17cc6541a4e82d084b399c0890b7c8b1985c3749
Chris Sayer <crichardsayer@gmail.com>
2022-11-12 11:56:40 -0500

    version 5

--- script.js
+++ script.js
@@ -93,14 +93,34 @@ createItemEl(backlogList, 0, backlogItem, index);
   createItemEl(onHoldList, 0, onHoldItem, index);
   });
   // Run getSavedColumns only once, Update Local Storage
+updateOnLoad = true;
+updateSavedColumns();
+}
 
-
+// Allows arrays to reflect drag and drop items 
+function rebuildArrays() {
+  backlogListArray = [];
+  for (let i = 0; i < backlogList.children.length; i++) {
+    backlogListArray.push(backlogList.children[i].textContent);
+  }
+  progressListArray = [];
+  for (let i = 0; i < progressList.children.length; i++) {
+    progressListArray.push(progressList.children[i].textContent);
+  }
+  completeListArray = [];
+  for (let i = 0; i < completeList.children.length; i++) {
+    completeListArray.push(completeList.children[i].textContent);
+  }
+  onHoldListArray = [];
+  for (let i = 0; i < onHoldList.children.length; i++) {
+    onHoldListArray.push(onHoldList.children[i].textContent);
+  }
+  updateDOM();
 }
 
 // When items starts dragging
 function drag(e) {
 draggedItem = e.target;
-console.log('draggedItem', draggedItem);
 }
 // Column Allows for Item to Drop
 function allowDrop(e) {
@@ -123,6 +143,7 @@ function drop(e) {
   // add item to column
 const parent = listColumns[currentColumn];
 parent.appendChild(draggedItem);
+rebuildArrays();
 }
 
 //  On Load
